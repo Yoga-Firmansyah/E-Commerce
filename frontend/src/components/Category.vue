37c2f8bc2afb32fc60f1c257de7b96fa23337be3
Yoga-Firmansyah <56305133+Yoga-Firmansyah@users.noreply.github.com>
2025-06-11 15:37:26 +0700

    Update Category.vue

--- frontend/src/components/Category.vue
+++ frontend/src/components/Category.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="card border-0 shadow rounded">
     <div class="card-body">
-      <h5 class="font-weight-bold" style="font-size: 4vmin">
+      <h5 class="font-weight-bold">
         <i class="fa fa-shopping-bag"></i> KATEGORI
       </h5>
       <hr />
-      <ul class="list-group list-group-horizontal" style="font-size: 3vmin">
+      <ul class="list-group list-group-horizontal" >
         <router-link
           :to="{ name: 'detail_category', params: { slug: category.slug } }"
-          class="list-group-item shadow-sm font-weight-bold text-decoration-none text-dark mr-2"
+          class="list-group-item shadow-sm font-weight-bold text-decoration-none text-dark mr-2 "
           style="text-align: center"
           v-for="category in categories"
           :key="category.id"
         >
-          <img :src="category.image" style="width: 5vmin" />
+          <img :src="category.image" style="width: 40px" />
           {{ category.name }}
           </router-link>
 
         <router-link :to="{name: 'categories'}"
-          class="list-group-item text-center active shadow-sm font-weight-bold text-decoration-none mr-2"
+          class="list-group-item text-center active shadow-sm font-weight-bold text-decoration-none mr-2 d-flex align-items-center"
           >LIHAT KATEGORI LAINNYA <i class="fa fa-long-arrow-alt-right"></i
         ></router-link>
       </ul>

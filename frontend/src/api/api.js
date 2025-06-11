89757b54aadbb60af18d56be58033b1d69446934
Yoga-Firmansyah <56305133+Yoga-Firmansyah@users.noreply.github.com>
2025-06-11 15:04:34 +0700

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://backend-ecommerce.yogafirmansyah.com/api',
 })
 
 export default Api
